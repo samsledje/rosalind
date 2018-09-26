e438eef5fa957bc9e6fb0bc7b4775dea6a7c241f
Sam <samsledje@gmail.com>
2018-09-26 19:14:41 -0400

    semiglobal fixed

--- SMGB.py
+++ SMGB.py
@@ -54,7 +54,7 @@ char_dict = {
 #     }
 # }
 
-f = open('datasets/test2.txt')
+f = open('datasets/rosalind_smgb.txt')
 seq = []
 for line in f:
     if line.startswith('>'):
@@ -70,28 +70,23 @@ n = len(y)
 
 M = np.zeros((m+1, n+1))
 
-for i in range(1, m):
-    for j in range(1, n):
+for i in range(1, m+1):
+    for j in range(1, n+1):
         x_ind = char_dict[x[i-1]]
         y_ind = char_dict[y[j-1]]
-        # M[(i, j)] = max(
-        #     M[(i-1, j)] + score_matrix[x[i-1]]['-'],
-        #     M[(i-1, j-1)] + score_matrix[x[i-1]][y[j-1]],
-        #     M[(i, j-1)] + score_matrix['-'][y[j-1]],
-        #     )
         M[(i, j)] = max(
             M[(i-1, j)] + score_matrix[(x_ind, 4)],
             M[(i-1, j-1)] + score_matrix[(x_ind, y_ind)],
             M[(i, j-1)] + score_matrix[(4, y_ind)],
             )
 
+print('single loops')
 for i in range(1, m+1):
     x_ind = char_dict[x[i-1]]
     y_ind = char_dict[y[n-1]]
     M[(i, n)] = max(
-        M[(i, n-1)],
+        M[(i, n-1)] + score_matrix[(x_ind, 4)],
         M[(i-1, n)],
-        # M[(i-1, n-1)] + score_matrix[x[i-1]][y[n-1]]
         M[(i-1, n-1)] + score_matrix[(x_ind, y_ind)]
         )
 
@@ -99,26 +94,34 @@ for j in range(1, n+1):
     x_ind = char_dict[x[m-1]]
     y_ind = char_dict[y[j-1]]
     M[(m, j)] = max(
-        M[(m-1, j)],
+        M[(m-1, j)] + score_matrix[(4, y_ind)],
         M[(m, j-1)],
-        # M[(m-1, j-1)] + score_matrix[x[m-1]][y[j-1]]
-        M[(i-1, n-1)] + score_matrix[(x_ind, y_ind)]
+        M[(m-1, j-1)] + score_matrix[(x_ind, y_ind)]
         )
+
+M[(m,n)] = max(M[(m-1, n)], M[(m-1,n-1)], M[(m,n-1)])
 
 i = m
 j = n
 x_prime = y_prime = ""
 
+print('backtracking')
 while i > 0 or j > 0:
     x_ind = char_dict[x[i-1]]
     y_ind = char_dict[y[j-1]]
-    # if M[i-1][j-1] + score_matrix[x[i-1]][y[j-1]] == M[i][j]:
-    if M[(i-1, j-1)] + score_matrix[(x_ind, y_ind)] == M[(i, j)]:
+    if (i == m or i == 0) and M[(i, j-1)] == M[(i, j)]:
+        x_prime = '-' + x_prime
+        y_prime = y[j-1] + y_prime
+        j -= 1
+    elif (j == n or j == 0) and M[(i-1, j)] == M[(i, j)]:
+        x_prime = x[i-1] + x_prime
+        y_prime = '-' + y_prime
+        i -= 1
+    elif M[(i-1, j-1)] + score_matrix[(x_ind, y_ind)] == M[(i, j)]:
         x_prime = x[i-1] + x_prime
         y_prime = y[j-1] + y_prime
         i -= 1
         j -= 1
-    # elif M[i][j-1] + score_matrix['-'][y[j-1]] == M[i][j]:
     elif M[(i, j-1)] + score_matrix[(4, y_ind)] == M[(i, j)]:
         x_prime = '-' + x_prime
         y_prime = y[j-1] + y_prime
@@ -135,7 +138,6 @@ while i > 0 or j > 0:
 # outfile.write('\n')
 # outfile.write(y_prime)
 # outfile.close()
-print(M)
 print(int(M[(m, n)]))
 print(x_prime)
 print(y_prime)
